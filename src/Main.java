9429adf6e8176b71fd844e17613b5b7efa2e1f73
Надя Трансвалева <liberavivi888@gmail.com>
2022-11-01 14:58:15 +0300

    Добавлен второй вызов метода сервисного класса

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import src.BonusMilesService;
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 10000;
+        int cost = 30000;
         int miles = service.calculate(cost);
-        System.out.println(miles);
+        System.out.println("За перелет из Санкт-Петербурга в Воронеж начислено " + miles + " миль");
+
+        System.out.println();
+        cost = 22500;
+        miles = service.calculate(cost);
+        System.out.println("За перелет из Москвы в Санкт-Петербург начислено " + miles + " миль");
     }
 }
